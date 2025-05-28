4a39308555f8099347081edefdb24ad61abd53de
Nicola Di Candia <nicoladicandiajaimes@gmail.com>
2025-05-28 19:00:20 -0400

    buttons functionality local storage and use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,85 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Guitar from "./components/Guitar";
 import Header from "./components/Header";
 import { db } from "./data/db";
 
 function App() {
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem("cart");
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  };
+
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState(initialCart);
+
+  const MIN_ITEMS = 1;
+  const MAX_ITEMS = 5;
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  function addToCart(item) {
+    const itemExist = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExist >= 0) {
+      if (cart[itemExist].quantity >= MAX_ITEMS) return;
+      const updatedCart = [...cart];
+      updatedCart[itemExist].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
+
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
+  }
+
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
+
+  function clearCart() {
+    setCart([]);
+  }
+
   return (
     <>
-      <Header />
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
+      />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
         <div className="row mt-5">
           {data.map((guitar) => (
-            <Guitar key={guitar.id} guitar={guitar} />
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
           ))}
         </div>
       </main>
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,8 +1,9 @@
-export default function Guitar({ guitar }) {
-  const { name, image, description, price } = guitar;
+export default function Guitar({ guitar, addToCart }) {
+  const { id, name, image, description, price } = guitar;
+
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-      <div className="col-4">
+      <div className="col-4" id={id}>
         <img
           className="img-fluid"
           src={`/img/${image}.jpg`}
@@ -13,7 +14,11 @@ export default function Guitar({ guitar }) {
         <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
         <p>{description}</p>
         <p className="fw-black text-primary fs-3">$ {price}</p>
-        <button type="button" className="btn btn-dark w-100 ">
+        <button
+          onClick={() => addToCart(guitar)}
+          type="button"
+          className="btn btn-dark w-100 "
+        >
           Agregar al Carrito
         </button>
       </div>
